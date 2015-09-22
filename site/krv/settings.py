94391a6dc25dc3808efa5f4a86983ca6b3bcb03b
Timo <timo.haimi@gmail.com>
2015-09-22 16:18:18 +0800

    restructure

--- site/krv/settings.py
+++ site/krv/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+
     'taggit',
     'registration',
 
@@ -82,7 +83,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '../static/'),
+)
 
 # Django-registration-redux
 
