312d880e30d8bd43cbb4038adf9ccb9fc2fdf631
Timo <timo.haimi@gmail.com>
2015-10-16 16:18:35 +0800

    shortcuts

--- site/planner/urls.py
+++ site/planner/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     url(r'^new/$', planner.views.PlanCreate.as_view(), name="new"),
     url(r'^edit/(?P<pk>[0-9]+)/$', planner.views.PlanUpdate.as_view(), name="edit"),
     
-    url(r'^(?P<slug>[-_\w]+)/$', planner.views.plan, name="plan"),
+    url(r'^(?P<plan>[-_\w]+)/$', planner.views.plan, name="plan"),
     url(r'^(?P<plan>[-_\w]+)/(?P<task>[-_\w]+)/$', planner.views.task, name="task"),
-    url(r'^(?P<slug>[-_\w]+)/new/(?P<type>[-_\w]+)/$', planner.views.TaskCreate.as_view(), name="task_create"),
+    url(r'^(?P<plan>[-_\w]+)/new/(?P<type>[-_\w]+)/$', planner.views.TaskCreate.as_view(), name="task_create"),
 
     url(r'^$', planner.views.index, name="index"),
 ]
---
+++ static/js/plan.js
@@ -0,0 +1,1 @@
+../../site/planner/static/js/plan.js
